28c30ba5f1a75b5c2ad6b9130fd6f7750f9e4183
Aaron Gwirtz <imac@iMacs-iMac.attlocal.net>
2020-11-25 10:18:48 -0500

    navbar router functional, component returns functions created

---
+++ react-client/src/components/Scorecards/Community-Scorecard/Community-Scorecard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+
+const CommunityScorecard = () => {
+ return (
+     <div className="main">
+        <div className="mainDiv">
+            <p>Community Scorecard Component</p>
+        </div>
+     </div>
+ )  ; 
+};
+
+export default CommunityScorecard;
