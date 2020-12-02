a727d7b4d36b132cb6a2f75f6b502a489035533b
Aaron Gwirtz <imac@iMacs-iMac.attlocal.net>
2020-12-02 16:19:05 -0500

    navbar update

--- react-client/src/Components/Scorecards/Pre-Scorecard/Pre-Scorecard.jsx
+++ react-client/src/Components/Scorecards/Pre-Scorecard/Pre-Scorecard.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import CreateScorecard from '../Create-Scorecard/Create-Scorecard';
 import golfer from '../../../assets/golfer.jpg';
 import '../Pre-Scorecard/Pre-Scorecard.css';
 
@@ -10,9 +9,6 @@ const PreScorecard = () => {
             <h2>It looks like you haven't created any scorecards yet!</h2>
             <h4>Click the Create New Scorecard link above to get started.</h4>
             <img id="golfer" src={golfer} alt="No Scorecards" />
-            {/* <CreateScorecard /> */}
-            {/* <button><Link to="scorecard/create">Create a Scorecard</Link></button> */}
-            {/* <button onClick = {handleClick}>Create a Scorecard!</button> */}
         </div>
     )
 }
