5d619667ba97216e12f74d9bcf8a72894b302285
caseyeschmann <72029934+caseyeschmann@users.noreply.github.com>
2020-12-02 09:37:24 -0500

    casey 12-2

--- react-client/src/Components/Scorecards/Pre-Scorecard/Pre-Scorecard.jsx
+++ react-client/src/Components/Scorecards/Pre-Scorecard/Pre-Scorecard.jsx
@@ -1,40 +1,22 @@
 import React from 'react';
-<<<<<<< HEAD
 // import golfer from '../assets/golfer';
 import CreateScorecard from '../Create-Scorecard/Create-Scorecard';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-const PreScorecard = () => {
 
-    const handleClick = (event) => {
-        event.preventdefault()
-        return<CreateScorecard />
-    }
-    return(
+function PreScorecard () {
+
+    return (
         <div>
             <h2>It looks like you haven't created any scorecards yet!</h2>
+
             {/* <img src={golfer} alt="No Scorecards" /> */}
-            {/* <button><Link to="scorecard/create">Create a Scorecard</Link></button> */}
-            {/* <button onClick = {handleClick}>Create a Scorecard!</button> */}
-            <CreateScorecard />
-=======
-import CreateScorecard from '../Create-Scorecard/Create-Scorecard'
 
-
-
-const PreScorecard = () => {
-
-    const handleClick = () => {
-      
-    }
-
- return (
-     <div className="main">
-        <div className="mainDiv">
-            <p>Pre Scorecard Component</p>
-            <button>Click me for new card</button>
->>>>>>> e41ccd976584be58c596df9a067a90dad40812c6
+            <Router>
+            <button onClick={ (e) => <CreateScorecard /> }>Create a Scorecard!</button>
+            </Router>
         </div>
     )
-}
+};
 
 export default PreScorecard;
