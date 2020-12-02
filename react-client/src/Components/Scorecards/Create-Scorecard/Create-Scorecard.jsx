d6b9e8ce5f324614f8a7e220f8c36614d8f63df2
Aaron Gwirtz <imac@iMacs-iMac.attlocal.net>
2020-12-02 11:18:12 -0500

    create scorecard commented out for display purposes

--- react-client/src/Components/Scorecards/Create-Scorecard/Create-Scorecard.jsx
+++ react-client/src/Components/Scorecards/Create-Scorecard/Create-Scorecard.jsx
@@ -51,18 +51,18 @@ const CreateScorecard = () => {
 
 
 
-        fetch('http://localhost:3000/scorecard/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': props.token
-            }
-         })
+        // fetch('http://localhost:3000/scorecard/create', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': props.token
+        //     }
+        //  })
 
-         const handleSubmit = (event) => {
-            fetchResults();
-            event.preventDefault();
-          }
+        //  const handleSubmit = (event) => {
+        //     fetchResults();
+        //     event.preventDefault();
+        //   }
 
  return (
      <div className="main">
@@ -70,219 +70,307 @@ const CreateScorecard = () => {
 
             <h1>Create Scorecard</h1>
                 <div id="myform">
-                <Form id="form1" onSubmit={(e) => handleSubmit(e)}>
+                <Form id="form1" 
+                // onSubmit={(e) => handleSubmit(e)}
+                >
                     <Row id="row1">
                         <Col>
                             <Label for="CourseName">Course Name</Label>
-                            <Input name="CourseName" type="text" onChange={(e) => setSearch(e.target.value)} />
+                            <Input name="CourseName" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)} 
+                            />
                         </Col>
                         <Col>
                             <Label for="DatePlayed">Date Played</Label>
-                            <Input name="DatePlayed" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="DatePlayed" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                         <Col>
                             <Label for="Conditions">Conditions</Label>
-                            <Input name="Conditions" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Conditions" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                         <Col>
                             <Label for="CourseLength">Course Length</Label>
-                            <Input name="CourseLength" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="CourseLength" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                         <Col>
                             <Label for="DifficultyRating">Difficulty Rating</Label>
-                            <Input name="DifficultyRating" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="DifficultyRating" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                     </Row>
 
                     <Row id="row2">
                         <Col>
                             <Label for="Par1">Par 1</Label>
-                            <Input name="Par1" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par1" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par2">Par 2</Label>
-                            <Input name="Par2" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par2" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par3">Par 3</Label>
-                            <Input name="Par3" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par3" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par4">Par 4</Label>
-                            <Input name="Par4" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par4" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par5">Par 5</Label>
-                            <Input name="Par5" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par5" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Par6">Par 6</Label>
-                            <Input name="Par6" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par6" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Par7">Par 7</Label>
-                            <Input name="Par7" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par7" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par8">Par 8</Label>
-                            <Input name="Par8" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par8" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Par9">Par 9</Label>
-                            <Input name="Par9" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par9" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par10">Par 10</Label>
-                            <Input name="Par10" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par10" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par11">Par 11</Label>
-                            <Input name="Par11" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par11" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Par12">Par 12</Label>
-                            <Input name="Par12" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par12" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par13">Par 13</Label>
-                            <Input name="Par13" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par13" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par14">Par 14</Label>
-                            <Input name="Par14" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par14" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par15">Par 15</Label>
-                            <Input name="Par15" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par15" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par16">Par 16</Label>
-                            <Input name="Par16" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par16" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par17">Par 17</Label>
-                            <Input name="Par17" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par17" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Par18">Par 18</Label>
-                            <Input name="Par18" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par18" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                         <Col>
                             <Label for="Par Total">Par Total</Label>
-                            <Input name="Par Total" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Par Total" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                     </Row>
 
                     <Row id="row3">
                         <Col>
                             <Label for="Hole1">Hole 1</Label>
-                            <Input name="Hole1" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole1" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole2">Hole 2</Label>
-                            <Input name="Hole2" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole2" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole3">Hole 3</Label>
-                            <Input name="Hole3" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole3" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole4">Hole 4</Label>
-                            <Input name="Hole4" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole4" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole5">Hole 5</Label>
-                            <Input name="Hole5" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole5" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Hole6">Hole 6</Label>
-                            <Input name="Hole6" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole6" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Hole7">Hole 7</Label>
-                            <Input name="Hole7" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole7" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole8">Hole 8</Label>
-                            <Input name="Hole8" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole8" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Hole9">Hole 9</Label>
-                            <Input name="Hole9" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole9" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole10">Hole 10</Label>
-                            <Input name="Hole10" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole10" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole11">Hole 11</Label>
-                            <Input name="Hole11" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole11" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                            
                         <Col>
                             <Label for="Hole12">Hole 12</Label>
-                            <Input name="Hole12" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole12" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole13">Hole 13</Label>
-                            <Input name="Hole13" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole13" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole14">Hole 14</Label>
-                            <Input name="Hole14" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole14" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole15">Hole 15</Label>
-                            <Input name="Hole15" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole15" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole16">Hole 16</Label>
-                            <Input name="Hole16" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole16" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole17">Hole 17</Label>
-                            <Input name="Hole17" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole17" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                             
                         <Col>
                             <Label for="Hole18">Hole 18</Label>
-                            <Input name="Hole18" type="text"  onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole18" type="text"  
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                         <Col>
                             <Label for="Hole Total">Hole Total</Label>
-                            <Input name="Hole Total" type="text" onChange={(e) => setSearch(e.target.value)}/>
+                            <Input name="Hole Total" type="text" 
+                            // onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
                     </Row>
                     {/* <Button onSubmit={(e) => {submitScore}}>Submit Your Score</Button> */}
